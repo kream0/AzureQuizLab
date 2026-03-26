8bfa295c83a32025c941549ee44145a3da7b0047
karimel <karim.elight@gmail.com>
2026-03-26 17:14:42 +0100

    TP2: Azure SQL, feature flag, EF Core, logging

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using AzureQuizLab.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,30 @@ namespace AzureQuizLab.Pages;
 
 public class IndexModel : PageModel
 {
+    private readonly QuizDbContext _context;
+    private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _configuration;
+
+    public bool MaintenanceMode { get; set; }
+    public int QuizCount { get; set; }
+    public int QuestionCount { get; set; }
+
+    public IndexModel(QuizDbContext context, ILogger<IndexModel> logger, IConfiguration configuration)
+    {
+        _context = context;
+        _logger = logger;
+        _configuration = configuration;
+    }
+
     public void OnGet()
     {
+        MaintenanceMode = _configuration.GetValue<bool>("MaintenanceMode", false);
 
+        QuizCount = _context.Quizzes.Count();
+        QuestionCount = _context.Questions.Count();
+
+        _logger.LogInformation("Chargement de la page d'accueil à {Time}", DateTime.UtcNow);
+        _logger.LogInformation("Nombre de quiz : {QuizCount}", QuizCount);
+        _logger.LogInformation("Nombre de questions : {QuestionCount}", QuestionCount);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
+using AzureQuizLab.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.AzureAppServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<QuizDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlOptions => sqlOptions.EnableRetryOnFailure()
+    ));
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddAzureWebAppDiagnostics();
 
 var app = builder.Build();
 
