d4e15225996347906734decf1901a1698c72187b
karimel <karim.elight@gmail.com>
2026-03-26 16:18:31 +0100

    Initial project

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AzureQuizLab.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
